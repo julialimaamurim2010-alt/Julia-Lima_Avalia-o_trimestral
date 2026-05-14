d9b7944778b14486738959ac98a5eb7d755f3383
DEV <DEV@NSN1191261182.edu119.local>
2026-05-14 16:49:56 -0300

    comentário

---
+++ Calculando_PA.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Vamos calcular uma PA!");
+Console.Write("Digite até que número você quer calcular a PA:");
+ int quantidade = int.Parse(Console.ReadLine());
+Console.Write("Digite a razão da PA:");
+    int razao = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= quantidade; i = i + razao)
+{
+   Console.Write($"{i}👉");
+}
+Console.Write("🔚Fim da PA!");
